a1c34326b08d5433aa5e9534f9e4e298ae231ac3
Milen Ivanov <m.ivanov@email.com>
2021-08-11 21:11:39 +0300

    build and design cards with data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,15 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { LoaderModule } from './loader/loader.module';
 import { CardComponent } from './card/card.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatChipsModule } from '@angular/material/chips';
 
 export const WindowToken = new InjectionToken<Window>('Window');
 export function windowProvider() { return window; }
@@ -21,6 +30,15 @@ export function windowProvider() { return window; }
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatChipsModule,
     LoaderModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  Component, OnInit, Inject, Input, ChangeDetectionStrategy,
+  ChangeDetectorRef, AfterViewInit
+} from '@angular/core';
+import { forkJoin, Observable } from 'rxjs';
+import { CardService } from '../card.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  isLoading = true;
+  avatarName?: string;
+  avatarStyle: Object = {};
+  @Input() title?: string;
+  @Input() img?: string;
+  @Input() description?: string;
+  @Input() logo?: string;
+  @Input() index: number = 0;
+
+  private title$?: Observable<string>;
+  private img$?: Observable<string>;
+  private description$?: Observable<string>;
+  private logo$?: Observable<string>;
+  private allData$?: Observable<[string, string, string, string]>;
+
+  constructor(@Inject(DOCUMENT) private doc: Document,
+    private cdr: ChangeDetectorRef,
+    private cs: CardService) { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+
+    const avatar = this.doc.getElementById(`avatar-${this.index}`);
+
+    if (this.title && this.img && this.description && this.logo && avatar) {
+
+      this.title$ = this.cs.get(this.title);
+      this.img$ = this.cs.get(this.img);
+      this.description$ = this.cs.get(this.description);
+      this.logo$ = this.cs.get(this.logo);
+
+      this.allData$ = forkJoin([this.title$, this.img$, this.description$, this.logo$]);
+
+      this.allData$
+        .subscribe(res => {
+          this.title = res[0];
+          this.description = res[2];
+
+          const img = new Image();
+
+          img.setAttribute('mat-card-image', '');
+          img.src = res[1];
+          img.style.width = '100%';
+          img.alt = `image ${this.index}`;
+
+          img.addEventListener('load', (e) => {
+
+            this.doc.getElementById(`img-${this.index}`)?.appendChild(img);
+
+            this.avatarName = res[3].replace(/[\.\/a-zA-Z0-9]+\//i, '').replace(/\.(svg|png)/i, '');
+            this.avatarStyle = {
+              'border-radius': '0%',
+              'background-image': `url(${res[3]})`
+            };
+
+            this.isLoading = false;
+            this.cdr.detectChanges();
+          });
+        });
+
+    }
   }
 
 }
