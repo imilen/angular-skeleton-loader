183d014a3b4c3c8af639b0a489cbe31211eea85e
Milen Ivanov <m.ivanov@email.com>
2021-08-11 19:57:01 +0300

    create CardService to simulate HTTP request

---
+++ src/app/card.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { delay, distinctUntilChanged } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardService {
+
+  title: string = '';
+  img: string = '';
+  description: string = '';
+  logo: string = '';
+
+  index: number = 0;
+
+  constructor() { }
+
+  get(str: string) {
+    return of(str).pipe(
+      delay(this.getRandomTimeout(100, 5000)),
+      distinctUntilChanged(),
+    )
+  }
+
+  private getRandomTimeout(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+  }
+
+}
